91ea9a237930cb6860d68a1e7da1a1e96060c11b
huyen2510 <buikhanhhuyena4@gmail.com>
2020-02-22 22:01:28 +0700

    add f_math

--- Buoi6/F_Math.py
+++ Buoi6/F_Math.py
@@ -3,29 +3,30 @@ from random import randint
 from Caculator import calc
 a =calc(1,2,'+')
 print(a)
-# a = randint(0,9)
-# b = randint(0,9)
-# d = randint(-1,1)
-# c = a + b
-# diem = 0
-# print( a ,"+", b ," = ", c+d)
-# ngnhap = input("1_Correct or 0_Incorrecr? ")
-# if (a + b == c +d) :
-#     if int(ngnhap) == 1 :
-#         print(" Đúng!")
-#         diem = diem + 1
-#         print( "Diem cua ban: ", diem)
-#     if int(ngnhap)== 0:
-#         print(" Sai! ")
-#         print(" Diem cua bạn: ", diem)
+a = randint(0,9)
+b = randint(0,9)
+d = randint(-1,1)
+c = a + b
+diem = 0
+print( a ,"+", b ," = ", c+d)
+ngnhap = input("1_Correct or 0_Incorrecr? ")
+if (a + b == c +d) :
+    if int(ngnhap) == 1 :
+        print(" Đúng!")
+        diem = diem + 1
+        print( "Diem cua ban: ", diem)
+    if int(ngnhap)== 0:
+        print(" Sai! ")
+        print(" Diem cua bạn: ", diem)
 
-# else:
-#     if int(ngnhap) == 1 :
-#         print(" Sai!")
-#         diem = diem + 1
-#         print( "Diem cua ban: ", diem)
-#     if int(ngnhap)== 0:
-#         print(" Đúng! ")
-#         diem = diem + 1
-#         print(" Diem cua bạn: ", diem)
+else:
+    if int(ngnhap) == 1 :
+        print(" Sai!")
+        diem = diem + 1
+        print( "Diem cua ban: ", diem)
+    if int(ngnhap)== 0:
+        print(" Đúng! ")
+        diem = diem + 1
+        print(" Diem cua bạn: ", diem)
     
+print("vfv")
